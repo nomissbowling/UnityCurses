9c37fc7589e254563376316b3186ca542af4e3bc
Maxwolf <ron.mcdowell@gmail.com>
2016-12-29 21:03:59 -0800

    Dumped in sources for wolf curses and it's example project into separate directories in the asset folder.

---
+++ Assets/Scripts/WolfCurses/Utility/DescriptionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WolfCurses.Utility
+{
+    public class DescriptionAttribute : Attribute
+    {
+        public DescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+
+        public string Description { get; set; }
+    }
+}
